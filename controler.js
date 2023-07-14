a37985cd9604e6b8663a314f291acbace20948e3
SUBHASH CHANDRA <121532236+subhashcse18@users.noreply.github.com>
2023-07-14 10:47:43 +0530

    Update controler.js

--- controler.js
+++ controler.js
@@ -1,11 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var button = document.getElementById("myButton");
-  button.addEventListener("click", function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      // Perform some action on the active tab
-      var activeTab = tabs[0];
-      console.log("Active tab:", activeTab);
-      // Add your logic here
-    });
+function clearTextbox() {
+    const textArea = document.getElementById("inputarea");
+    textArea.value = "";
+  }
+  function run() {
+    let textarea = document.getElementById("inputarea").value;
+    let url = 'https://api.openai.com/v1/chat/completions';
+    let data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [
+        {
+            "role": "user",
+            "content": textarea
+        }
+    ],
+    "temperature": 0.7
+};
+
+async function getData(url) {
+await fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer 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'
+  },
+  body: JSON.stringify(data)
+})
+.then(response =>{ 
+    response.json()})
+.then(response => {
+    return JSON.stringify(response)})
+  .catch(error => {
+    console.error('Error:', error);
   });
-});
+};
+
+let setOutput = document.getElementById("outputtext");
+setOutput.value = getData(url);
+
+  }
